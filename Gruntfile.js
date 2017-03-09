147b3445b5da78a2f1b93fcdae5d9a42afbb1639
Darin Allen <darinallen@gmail.com>
2017-03-09 18:00:03 -0500

    Set up Grunt file and tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,104 +1,127 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        seperator: ';',
-      },
-      dist:
-    },
-
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                seperator: ';',
+            },
+            dist: {
+                src: ['public/client/**/*.js'],
+                dest: 'public/dist/<%= pkg.name %>.js',
+            }
         },
-        src: ['test/**/*.js']
-      }
-    },
 
-    nodemon: {
-      dev: {
-        script: 'server.js'
-      }
-    },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/**/*.js']
+            }
+        },
 
-    uglify: {
-      my_target: {
-        files: {
-          'dest/output.min.js': ['src/input1.js', 'src/input2.js']
+        nodemon: {
+            dev: {
+                script: 'server.js'
+            }
+        },
+
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+          },
+          dist: {
+            files: {
+              'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+            }
+          }
+        },
+
+        eslint: {
+            target: [
+                'public/dist/<%= pkg.name %>.js'
+            ]
+        },
+
+        cssmin: {
+          options: {
+            keepSpecialComments: 0
+          },
+          dist: {
+            files: {
+              'public/dist/style.min.css': 'public/style.css'
+            }
+          }
+        },
+
+        watch: {
+            scripts: {
+                files: [
+                    'public/client/**/*.js',
+                    'public/lib/**/*.js',
+                ],
+                tasks: [
+                    'concat',
+                    'uglify'
+                ]
+            },
+            css: {
+                files: 'public/*.css',
+                tasks: ['cssmin']
+            }
+        },
+
+        shell: {
+            prodServer: {
+              command: 'git push live master',
+              options: {
+                stdout: true,
+                stderr: true,
+                failOnError: true
+              }
+                    }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    grunt.registerTask('server-dev', function(target) {
+        grunt.task.run(['nodemon', 'watch']);
+    });
+
+    ////////////////////////////////////////////////////
+    // Main grunt tasks
+    ////////////////////////////////////////////////////
+
+    grunt.registerTask('test', [
+        'eslint',
+        'mochaTest'
+    ]);
+
+    grunt.registerTask('build', ['concat', 'uglify', 'cssmin']);
+
+    grunt.registerTask('upload', function(n) {
+        if (grunt.option('prod')) {
+            grunt.task.run(['shell:prodServer']);// add your production server task here
+        } else {
+            grunt.task.run(['server-dev']);
         }
-      }
-    },
+    });
 
-    eslint: {
-      target: [
-        // Add list of files to lint here
-      ]
-    },
-
-    cssmin: {
-    },
-
-    watch: {
-      scripts: {
-        files: [
-          'public/client/**/*.js',
-          'public/lib/**/*.js',
-        ],
-        tasks: [
-          'concat',
-          'uglify'
-        ]
-      },
-      css: {
-        files: 'public/*.css',
-        tasks: ['cssmin']
-      }
-    },
-
-    shell: {
-      prodServer: {
-      }
-    },
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
-
-  grunt.registerTask('server-dev', function (target) {
-    grunt.task.run([ 'nodemon', 'watch' ]);
-  });
-
-  ////////////////////////////////////////////////////
-  // Main grunt tasks
-  ////////////////////////////////////////////////////
-
-  grunt.registerTask('test', [
-    'mochaTest'
-  ]);
-
-  grunt.registerTask('build', [
-  ]);
-
-  grunt.registerTask('upload', function(n) {
-    if (grunt.option('prod')) {
-      // add your production server task here
-    } else {
-      grunt.task.run([ 'server-dev' ]);
-    }
-  });
-
-  grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
+    grunt.registerTask('deploy', [
+        'test',
+        'build',
+        'upload'
+    ]);
 
 
 };
